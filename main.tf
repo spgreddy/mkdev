458bb68bd36e19d9e64b43d48b9cb49cd6fa358e
Siva Reddy <sivaprasadreddyg@gmail.com>
2023-08-08 10:54:22 +0200

    main.tf

---
+++ main.tf
@@ -0,0 +1,83 @@
+# main.tf
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# Create a VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block = var.vpc_cidr
+}
+
+# Create public and private subnets across multiple AZs
+resource "aws_subnet" "public_subnets" {
+  count                   = var.num_public_subnets
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  vpc_id                  = aws_vpc.main_vpc.id
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+}
+
+resource "aws_subnet" "private_subnets" {
+  count                   = var.num_private_subnets
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  vpc_id                  = aws_vpc.main_vpc.id
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+}
+
+# Define security groups for isolation
+resource "aws_security_group" "product_security_group" {
+  name_prefix = "product-sg-"
+  # Define the rules here to enforce network-level isolation
+  # e.g., allow communication within the product's services, restrict access from other products, etc.
+}
+
+# Create IAM roles and policies for access control
+# Define the IAM roles and policies to grant teams access to their respective resources only
+
+# Create an EKS cluster
+# Define the EKS cluster configuration, node groups, scaling, resiliency, etc.
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_eks_cluster" "example" {
+  name     = var.eks_cluster_name
+  role_arn = aws_iam_role.eks_cluster.arn
+
+  vpc_config {
+    subnet_ids = var.public_subnet_ids
+  }
+
+  depends_on = [aws_iam_role_policy_attachment.eks_cluster]
+}
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+      Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+# Set up managed databases
+# Create RDS instances for teams that require relational data storage
+# Make sure to create them within the appropriate VPC and subnet
+
+# Use S3 for object storage
+# Define S3 buckets and access controls to ensure isolation between products
+
+# Use Route 53 for DNS management
+# Implement DNS records for new products and services using the AWS CLI or SDKs
